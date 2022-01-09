74b23c0b440f46df122b04ed63b966d420cf1d26
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-01-09 23:21:57 +0900

    fix: chapter19

---
+++ chapter19/fac.ml
@@ -0,0 +1,21 @@
+(*2分探索木を表すモジュール *)
+
+module Tree = struct
+  (*2分探索木を表す型*)
+  type ('a, 'b) t = Empty | Node of ('a, 'b) t * 'a * 'b * ('a, 'b) t
+
+  (*空の木 *)
+  let empty = Empty
+
+  (*目的　treeにキーがkで値がvを挿入した木を返す *)
+  let rec insert tree k v = match tree with
+  Empty -> Node (Empty, k, v, Empty) | Node(left, key, value, right) ->
+    if k = key then Node (left, key, v, right)
+    else if k < key
+      then Node (insert left k v, key, value, right)
+else if k < key
+  then Node (insert left k v, key, value, right)
+else Node (left, key, value, insert right k v)
+
+
+
